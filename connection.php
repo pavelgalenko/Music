7b276e8d1d0b2e1d1591ab766ca021b5e7dbf9be
Pavel Galenko <galenkopavel@gmail.com>
2022-08-21 19:54:37 +0700

    made connection file to db

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    $HOST = "localhost";
+    $USER = "root";
+    $PASSWORD = "root";
+    $DB = "music";
+    $connection = new mysqli($HOST, $USER, $PASSWORD, $DB);
+    //$connection -> query('SET NAMES "utf8"');
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once('connection.php');
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
