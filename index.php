6aefb889955562b6d142be198036af75d10b2281
Pavel Galenko <galenkopavel@gmail.com>
2022-08-22 12:24:59 +0700

    delete albums

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    require_once('connection.php');
+    require_once('connection.php'); //connect to database
+    $query = $connection -> query("SELECT albums.id, albums.title, albums.rating, albums.cover, artists.name FROM albums 
+        JOIN artists ON albums.id_artist = artists.id");
 ?>
 
 <!DOCTYPE html>
@@ -23,89 +25,7 @@
                     <img src="img/covers/chinese.jpeg" alt="album-image"/>
                 </a>
             </div>
-            <div class="rating r-5">
-
-            </div>
-            <h3>
-                <a href="#">Chinese Football</a>
-            </h3>
-            <h4>
-                <a href="#">Chinese Football</a>
-            </h4>
-        </div>
-        <div class="album">
-            <div class="album-image">
-                <a href="#">
-                    <img src="img/covers/chinese.jpeg" alt="album-image"/>
-                </a>
-            </div>
-            <div class="rating r-4">
-
-            </div>
-            <h3>
-                <a href="#">Chinese Football</a>
-            </h3>
-            <h4>
-                <a href="#">Chinese Football</a>
-            </h4>
-        </div>
-        <div class="album">
-            <div class="album-image">
-                <a href="#">
-                    <img src="img/covers/chinese.jpeg" alt="album-image"/>
-                </a>
-            </div>
-            <div class="rating r-5">
-
-            </div>
-            <h3>
-                <a href="#">Chinese Football</a>
-            </h3>
-            <h4>
-                <a href="#">Chinese Football</a>
-            </h4>
-        </div>
-        <div class="album">
-            <div class="album-image">
-                <a href="#">
-                    <img src="img/covers/chinese.jpeg" alt="album-image"/>
-                </a>
-            </div>
-            <div class="rating r-5">
-
-            </div>
-            <h3>
-                <a href="#">Chinese Football</a>
-            </h3>
-            <h4>
-                <a href="#">Chinese Football</a>
-            </h4>
-        </div>
-        <div class="album">
-            <div class="album-image">
-                <a href="#">
-                    <img src="img/covers/chinese.jpeg" alt="album-image"/>
-                </a>
-            </div>
-            <div class="rating r-5">
-
-            </div>
-            <h3>
-                <a href="#">Chinese Football</a>
-            </h3>
-            <h4>
-                <a href="#">Chinese Football</a>
-            </h4>
-        </div>
-        <div class="album">
-            <div class="album-image">
-                <a href="#">
-                    <img src="img/covers/chinese.jpeg" alt="album-image"/>
-                </a>
-            </div>
-            <div class="rating r-5">
-
-            </div>
+            <div class="rating r-5"></div>
             <h3>
                 <a href="#">Chinese Football</a>
             </h3>
